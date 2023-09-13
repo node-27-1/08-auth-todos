b3771e3b9dab8ca39b32b1b51ba35f929c14b4c6
Andres Mendoza <andres.david.mm@hotmail.com>
2023-09-12 21:46:04 -0500

    first commit

---
+++ src/models/ToDo.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const ToDo = sequelize.define('toDo', {
+  task: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  isCompleted: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+});
+
+module.exports = ToDo;
---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+User.prototype.toJSON = function () {
+  const values = Object.assign({}, this.get());
+  delete values.password;
+  return values;
+}
+
+module.exports = User;
